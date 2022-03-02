6eadfa96c48cead5bab0d96441a363dabcf5bb41
AfanasevEvgeny <afazhenya2001@gmail.com>
2022-03-03 00:36:11 +0300

    webpack config

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import {App} from "./App"
+
+const root = document.getElementById('app')
+
+ReactDom.render(<App/>, root)
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: "bundle.js"
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                },
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ]
+            }
+        ]
+    }
+}
