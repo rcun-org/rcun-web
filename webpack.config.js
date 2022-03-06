61306ce283e39c69499656a9287643af8ad9afad
Evgeny Afanasyev <ysafan13@mts.ru>
2022-03-06 15:39:52 +0300

    Improved webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,49 @@
-const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const {template} = require("@babel/core");
+
+let mode = "development"
+
+if (process.env.NODE_ENV === "production") {
+    mode = "production"
+}
 
 module.exports = {
-    entry: './src/index.js',
-    mode: 'development',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: "bundle.js"
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'public'),
-        },
-        compress: true,
-        port: 9000,
-    },
+    mode: mode,
+    target: 'web',
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
-                },
+                }
             },
             {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ]
+                test: /\.s?css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset"
             }
         ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin(
+            {
+                template: "./src/index.html"
+            }
+        )
+    ],
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: "./dist",
+        hot: true
     }
 }
