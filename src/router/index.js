36877930119bb122aa7484005e71e13623e973c3
Evgeny Afanasyev <ysafan13@mts.ru>
2022-03-19 00:39:41 +0300

    rename videoPlayer page to Room

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import AppMain from "../pages/AppMain";
-import VideoPlayer from "../pages/VideoPlayer";
-
+import Room from "../pages/Room";
 // export const privateroutes = [ todo after login feature done, by now all stuff will be public
 //     {path: '/', component: Rooms, exact: true},
 //     {path: '/video/:id', component: Video, exact: true},
@@ -9,5 +8,5 @@ import VideoPlayer from "../pages/VideoPlayer";
 export const publicRoutes = [
     // {path: '/login', component: Login, exact: true}, todo after login feature done, by now all stuff will be public
     {path: '/', component: AppMain, exact:true},
-    {path: '/video/:id', component: VideoPlayer, exact:true}
+    {path: '/room/:id', component: Room, exact:true}
 ]
