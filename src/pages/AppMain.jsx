2e57fd70921b3d97663ba925ef0c76839d1c0fbf
Evgeny Afanasyev <ysafan13@mts.ru>
2022-03-19 14:46:05 +0300

    Video preview cards added

--- src/pages/AppMain.jsx
+++ src/pages/AppMain.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import YouTube from "react-youtube";
+import AppMainContainer from "../components/AppMain/AppMainContainer";
 
 const AppMain = () => {
-    var opts = {
-        width: '360',
-        height: '240',
-    };
     return (
-        <div>
-            Main
-            <YouTube
-                videoId="2g811Eo7K8U"
-                opts={opts}
-                onReady={() => console.log("youtube ready")}
-            />
-        </div>
+        <AppMainContainer/>
     );
 };
 
--- src/pages/VideoPlayer.jsx
+++ src/pages/VideoPlayer.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import YouTube from "react-youtube";
 
 const VideoPlayer = () => {
+    const opts = {
+        width: '360',
+        height: '240',
+    };
     return (
         <div>
-            Video player
+            video
+            <YouTube
+                videoId="2g811Eo7K8U"
+                opts={opts}
+                onReady={() => console.log("youtube ready")}
+            />
         </div>
     );
 };
