524bf98ca8666a545ca9282515f33b19a03530f3
al1ych <timur.aladdinych@gmail.com>
2022-03-19 11:51:37 +0300

    add react-youtube dependency add youtube tag to main page

--- src/pages/AppMain.jsx
+++ src/pages/AppMain.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import YouTube from "react-youtube";
 
 const AppMain = () => {
+    var opts = {
+        width: '360',
+        height: '240',
+    };
     return (
         <div>
             Main
+            <YouTube
+                videoId="2g811Eo7K8U"
+                opts={opts}
+                onReady={() => console.log("youtube ready")}
+            />
         </div>
     );
 };
