a8492173abedc9d71a897a74da1bbabde3f8df30
romanza705 <r.mukhtarov@innopolis.university>
2022-04-01 12:25:27 +0300

    add new route

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import AppMain from "../pages/AppMain";
 import Room from "../pages/Room";
 import Login from "../pages/Login";
+import CreateRoom from "../pages/CreateRoom";
 
 export const privateRoutes = [
     {path: '/', component: AppMain, exact: true},
     {path: '/room/:id', component: Room, exact: true},
+    {path: '/new_room', component: CreateRoom, exact: true}
 ]
 
 export const publicRoutes = [
