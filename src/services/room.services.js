596eb30dd150a436fc8bbcb90ac54ae5dce70f81
al1ych <t.nugaev@innopolis.university>
2022-04-10 19:25:11 +0300

    room create now transforms link preparing it for backend

--- src/services/room.services.js
+++ src/services/room.services.js
@@ -1,8 +1,19 @@
 import axios from "axios";
 import authHeader from './auth-header'
 
+function prepareData(s) {
+    if (s.indexOf("?v=") == -1) {
+        alert("Invalid link :c");
+        return "";
+    }
+    s = s.substring(s.indexOf("?v=") + 3);
+    console.log("yt video id transformed:", s);
+    return s;
+}
+
 const API_URL = process.env["REACT_APP_API_SERVER"]
 export const createRoom = async ({title, yt_video_id}) => {
+    yt_video_id = prepareData(yt_video_id);
     try {
         const room = await axios.post(API_URL + 'room',
             {title, yt_video_id},
