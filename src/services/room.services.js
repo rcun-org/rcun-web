fbef286e9aa05296892e220727d6bbdda523e33b
Timur Nugaev <decter49pro@gmail.com>
2022-04-11 00:04:32 +0300

    adapt preparation of data for backend now we use links, not video ids

--- src/services/room.services.js
+++ src/services/room.services.js
@@ -1,17 +1,17 @@
 import axios from "axios";
-import authHeader from './auth-header'
+import authHeader from './auth-header';
 
 function prepareData(s) {
     if (s.indexOf("?v=") === -1) {
         alert("Invalid link :c");
         return "";
     }
-    s = s.substring(s.indexOf("?v=") + 3);
-    s = s.split('&')[0]
+    // s = s.substring(s.indexOf("?v=") + 3);
+    // s = s.split('&')[0];
     return s;
 }
 
-const API_URL = process.env["REACT_APP_API_SERVER"]
+const API_URL = process.env["REACT_APP_API_SERVER"];
 export const createRoom = async ({title, yt_video_id}) => {
     yt_video_id = prepareData(yt_video_id);
     try {
@@ -20,31 +20,34 @@ export const createRoom = async ({title, yt_video_id}) => {
             {
                 headers: authHeader(),
             }
-        )
-        return room.data
-    } catch (e) {
-        return false
+        );
+        return room.data;
     }
-}
+    catch (e) {
+        return false;
+    }
+};
 
 export const getRooms = async () => {
     try {
         const roomsData = await axios.get(API_URL + 'room', {
             headers: authHeader(),
-        })
+        });
 
-        return roomsData.data
-    } catch (e) {
+        return roomsData.data;
+    }
+    catch (e) {
 
     }
-}
+};
 
 export const getRoomById = async (roomId) => {
     try {
-        const roomData = await axios.get(`${API_URL}room/${roomId}`)
-        return roomData.data
-    }catch (e) {
+        const roomData = await axios.get(`${API_URL}room/${roomId}`);
+        return roomData.data;
+    }
+    catch (e) {
 
     }
-}
+};
 
