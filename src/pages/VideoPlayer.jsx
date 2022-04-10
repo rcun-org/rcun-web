940d9ffb4fd6cf7429a19787647bdc3628af2dcd
Timur Nugaev <decter49pro@gmail.com>
2022-04-11 00:01:33 +0300

    make adaptive video player component ; changed video player type from react-youtube to react-player

--- src/pages/VideoPlayer.jsx
+++ src/pages/VideoPlayer.jsx
@@ -1,19 +1,23 @@
 import React, {useState} from 'react';
-import YouTube from "react-youtube";
+import ReactPlayer from "react-player";
 import {useParams} from "react-router-dom";
 
 const VideoPlayer = ({videoId}) => {
-    const [roomData, setRoomData] = useState()
+    const [roomData, setRoomData] = useState();
     const opts = {
         width: '640',
         height: '480',
     };
     return (
         <div>
-            <YouTube
-                videoId={videoId}
-                opts={opts}
-                onReady={() => console.log("youtube ready")}
+            <ReactPlayer
+                url="https://www.youtube.com/watch?v=dz3v0EY_Msg"
+                // videoId={videoId}
+                // opts={opts}
+                // onReady={() => console.log("youtube ready")}
+                width='100%'
+                height='100%'
+                controls={true}
             />
         </div>
     );
