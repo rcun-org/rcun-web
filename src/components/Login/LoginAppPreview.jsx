1365f4acca36ea1c04d59c41dfecb444ac929d0b
al1ych <decter49pro@gmail.com>
2022-12-04 20:54:57 +0300

    Fix launching bugs + login preview page

--- src/components/Login/LoginAppPreview.jsx
+++ src/components/Login/LoginAppPreview.jsx
@@ -4,7 +4,7 @@ const LoginAppPreview = (props) => {
     return (
         <div className={props.className}>
             <h1>Welcome to RCUN</h1>
-            <h2>If you want to have a great time watching videos with your friends, you'll like it here 🙌🏻</h2>
+            <h2>This is where you can have a great time watching videos with your friends 🙌🏻</h2>
         </div>
     );
 };
