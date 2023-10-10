152ee0e9ade086e64ffb2e2fbd964cfab0466311
Nikita Averochkin <dota.nikita@inbox.ru>
2023-10-10 21:20:46 +0300

    refactored UI components + fixed bug with unavailable className

--- src/components/UI/Button/BaseButton.jsx
+++ src/components/UI/Button/BaseButton.jsx
@@ -1,27 +1,31 @@
-import React from "react"
-import classes from "./BaseButton.module.scss"
-import { useState, useEffect } from "react"
+import React from "react";
+import classes from "./BaseButton.module.scss";
+import { useState, useEffect } from "react";
 
-const BaseButton = props => {
-  let [isFocused, setIsFocused] = useState(false)
-  let c = document.getElementById("sub-cursor")
+import cx from "classnames";
+
+const BaseButton = (props) => {
+  const { className, ...restProps } = props || {};
+
+  let [isFocused, setIsFocused] = useState(false);
+  let c = document.getElementById("sub-cursor");
 
   useEffect(() => {
     if (isFocused) {
-      c.classList.add("cursor-over-button")
+      c.classList.add("cursor-over-button");
     } else {
-      c.classList.remove("cursor-over-button")
+      c.classList.remove("cursor-over-button");
     }
-  }, [isFocused])
+  }, [isFocused]);
 
   return (
     <button
-      {...props}
-      className={classes.baseButton}
-      onMouseEnter={e => setIsFocused(true)}
-      onMouseLeave={e => setIsFocused(false)}
+      {...restProps}
+      className={cx(classes.baseButton, className)}
+      onMouseEnter={(e) => setIsFocused(true)}
+      onMouseLeave={(e) => setIsFocused(false)}
     />
-  )
-}
+  );
+};
 
-export default BaseButton
+export default BaseButton;
